43572c151cc600610475c0bc3e06c3bc11abb1dc
Priyanshu <priyansgupta17@gmail.com>
2026-06-21 12:40:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Display an alert when the page loads
+
 window.onload = function() {
   alert("Welcome to PG Stream! 🚧\n\nPlease note: This is not a finished project yet. It is a practice project being built for learning purposes.");
 };
