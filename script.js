0ad1820b4c5765bd56da0f71994f263e10300704
priyans16PG <priyansgupta17@gmail.com>
2026-06-22 12:33:13 +0530

    update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-// window.onload = function () {
-//     alert("Welcome to PG Stream! 🚧\n\nPlease note: This is not a finished project yet. It is a practice project being built for learning purposes.");
-// };
+window.onload = function () {
+    alert("Welcome to PG Stream! 🚧\n\nPlease note: This is not a finished project yet. It is a practice project being built for learning purposes.");
+};
